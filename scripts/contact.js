7db302af845fabcb28ee769b8de9f633e0297db1
Iryna <ira.14548@gmail.com>
2024-02-03 12:52:59 +0100

    update information

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const hostingPath = 'http://localhost:3006';
+const HOSTING_PATH = 'https://foocoding-portfolio-server.onrender.com';
 
 const initializeContactForm = () => {
     const contactForm = document.getElementById('js-contactForm');
@@ -8,38 +8,41 @@ const initializeContactForm = () => {
     if (contactForm) {
         contactForm.addEventListener('submit', (event) => {
             event.preventDefault();
-            const formData = new FormData(contactForm);
-            const name = formData.get('name');
-            const email = formData.get('email');
-            const phone = formData.get('phone');
-            const message = formData.get('message');
-
-            const data = {
-                name: name,
-                email: email,
-                phone: phone,
-                message: message,
-                siteId: 'Iryna',
-            };
-
-            fetch(hostingPath + '/contact', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(data),
-            }).then(async (response) => {
-                // ToDo:
-                // Contact was submitted OK.
-            });
+            submitContactForm(contactForm);
         });
     }
 };
 
-const submitContactForm = async (event) => {
-    // ToDo
+const submitContactForm = async (contactForm) => {
+    const formData = new FormData(contactForm);
+    const name = formData.get('name');
+    const email = formData.get('email');
+    const phone = formData.get('phone');
+    const message = formData.get('message');
+
+    const data = {
+        name: name,
+        email: email,
+        phone: phone,
+        message: message,
+        siteId: 'Iryna',
+    };
+
+    fetch(HOSTING_PATH + '/contact', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    }).then(async (response) => {
+        // ToDo:
+        // Show snackbar with OK message
+        // Clear form
+    });
 };
 
 const getAllContactForms = async () => {
-    // ToDo
+    fetch(HOSTING_PATH + '/contacts?siteId=Iryna', {
+        method: 'GET',
+    }).then(async (response) => {});
 };
